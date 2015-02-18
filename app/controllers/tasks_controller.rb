c4d6d05b57e2a44f633f5bf6b90ed257e383d77b
Annihilous <benjaminjnorton@gmail.com>
2015-02-18 09:51:41 -0500

    js files complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :all_tasks, only: [:index, :create]
+  before_action :all_tasks, only: [:index, :create, :update, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def new
@@ -8,6 +8,14 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.create(task_params)
+  end
+
+  def update
+    @task.update(task_params)
+  end
+
+  def destroy
+    @task.destroy
   end
 
   private
